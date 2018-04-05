200c5f7e153a3dbb49e57db6ca92c410058eb2aa
Nikolay Gelemerov <nikolay.gelemerov@imperiaonline.org>
2018-04-05 18:02:04 +0300

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+const Index = () => (
+    <div>
+        <Layout>
+            <h1>About BitzPrice</h1>
+        </Layout>
+    </div>
+);
+
+export default Index;
